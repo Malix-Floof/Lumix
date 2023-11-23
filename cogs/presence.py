b2b267c784eea76676c8109aaf71b39a62575ef5
Malix <101697781+Malix-Dev@users.noreply.github.com>
2023-11-23 23:34:13 +0300

    Форматирование серверов в присутствии (1.1k и т.д)

--- cogs/presence.py
+++ cogs/presence.py
@@ -1,19 +1,23 @@
-from disnake import Activity, ActivityType
+import disnake
 from disnake.ext import commands, tasks
-
 
 class Presence(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
     @commands.Cog.listener()
     async def on_ready(self):
         self.update_stats.start()
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(minutes=1)
     async def update_stats(self):
-        await self.bot.change_presence(activity=Activity(name=f'за {len(self.bot.guilds)} серверами', type=ActivityType.watching))
-
+        servers = len(self.bot.guilds)
+        serverstr = (
+            str(servers) if servers < 1000
+            else f'{round(servers / 1000, 1)}k' if servers < 1000000
+            else f'{round(servers / 1000000, 1)}M'
+        )
+        await self.bot.change_presence(activity=disnake.Activity(name=f'за {serverstr} серверами', type=disnake.ActivityType.watching))
 
 def setup(bot):
     bot.add_cog(Presence(bot))
