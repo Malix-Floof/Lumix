0baf80285f992c7e861c57c3778bea881fae3a63
Malix <101697781+Malix-Dev@users.noreply.github.com>
2023-11-24 13:15:29 +0300

    Оптимизация мусора

--- utils/constant.py
+++ utils/constant.py
@@ -1,46 +1,40 @@
-import re
 from disnake.ui import Button, Select
 from disnake import ButtonStyle
 from config import emojis
 from .classes import *
 
-URL_REG = re.compile(r'https?://(?:www\.)?.+')
 TRACK_POSITION_EMBED_EMOJI = "<:5_:1083430173900292178>"
 TRACK_LEFT_EMOJI = "<:3_:1083430185761783878>"
 
 CONTROLLER_BUTTON = [
     Button(custom_id=PlayerControls.BACK,
            emoji=emojis['backEmoji'],
-           style=ButtonStyle.gray,
-           row=0),
+           style=ButtonStyle.gray),
     Button(custom_id=PlayerControls.PAUSE,
            emoji=emojis['pauseEmoji'],
-           style=ButtonStyle.gray,
-           row=0),
+           style=ButtonStyle.gray),
     Button(custom_id=PlayerControls.SKIP,
            emoji=emojis['skipEmoji'],
-           style=ButtonStyle.gray,
-           row=0),
-    Button(custom_id=PlayerControls.STOP,
-           emoji=emojis['stopEmoji'],
-           style=ButtonStyle.gray,
-           row=0),
-    Button(custom_id=PlayerControls.SHUFFLE,
-           emoji=emojis['shuffleEmoji'],
-           style=ButtonStyle.gray),
-    Button(custom_id=PlayerControls.LOOP_MODE,
-           emoji=emojis['loopEmoji'],
-           style=ButtonStyle.gray),
-    Button(custom_id=PlayerControls.VOLUMEM,
-           emoji=emojis['volumemEmoji'],
-           style=ButtonStyle.gray),
-    Button(custom_id=PlayerControls.VOLUMEP,
-           emoji=emojis['volumepEmoji'],
            style=ButtonStyle.gray),
     Button(custom_id=PlayerControls.PLAYLIST,
            emoji=emojis['playlistEmoji'],
            style=ButtonStyle.gray),
+    Button(custom_id=PlayerControls.VOLUMEP,
+           emoji=emojis['volumepEmoji'],
+           style=ButtonStyle.gray),
+    Button(custom_id=PlayerControls.SHUFFLE,
+           emoji=emojis['shuffleEmoji'],
+           style=ButtonStyle.gray),
+    Button(custom_id=PlayerControls.STOP,
+           emoji=emojis['stopEmoji'],
+           style=ButtonStyle.gray),
+    Button(custom_id=PlayerControls.LOOP_MODE,
+           emoji=emojis['loopEmoji'],
+           style=ButtonStyle.gray),
     Button(custom_id=PlayerControls.BASSBOOST,
            emoji=emojis['bassboostEmoji'],
+           style=ButtonStyle.gray),
+    Button(custom_id=PlayerControls.VOLUMEM,
+           emoji=emojis['volumemEmoji'],
            style=ButtonStyle.gray),
 ]
