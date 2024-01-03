3e9d348f7e81cd620ca5e2e88c6f4c1160362ab2
Malix <101697781+Malix-Dev@users.noreply.github.com>
2024-01-03 14:00:43 +0300

    Update error_handler.py

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,4 +1,29 @@
+"""
+MIT License
+
+Copyright (c) 2022 - 2024 Tasfers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import disnake
+
 from disnake.ext import commands
 from datetime import datetime
 
@@ -8,15 +33,9 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_slash_command_error(self, inter, error):
-        if isinstance(error, commands.Cooldown):
-            return
         channel = self.bot.get_channel(1149329202777170022)
         timestamp = int(datetime.timestamp(datetime.now()))
         command = inter.permissions.value
-        if command == 562949953421311:
-            command = "562949953421311 - Все права"
-        if command == 8:
-            command = "8 - Администратор"
         name = inter.application_command.name
         embed = disnake.Embed(description=f"`💔` <t:{timestamp}:f> (<t:{timestamp}:R>) Последний вызов вызвал исключение:\n```cmd\n{error}\n```"
                                           f"\nКод разрешений: \n```\n{command}\n```\nСервер: {inter.guild.name}\nПользователь: {inter.author.name}\nID сервера: {inter.guild.id}\nID участника: {inter.author.id}"
