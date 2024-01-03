065af64c1cd1d7fa282ef6370e62c217bb9e8af0
Malix <101697781+Malix-Dev@users.noreply.github.com>
2024-01-03 14:05:58 +0300

    Update logs.py

--- cogs/logs.py
+++ cogs/logs.py
@@ -1,25 +1,40 @@
+"""
+MIT License
+
+Copyright (c) 2022 - 2024 Tasfers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import datetime
 import disnake
+
 from disnake.ext import commands
 from db import SQLITE
 from datetime import datetime
-import datetime
+
 
 db = SQLITE("database.db")
 
 class Logs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    # [ ] Трибуна обновлена
-    # [ ] Участник покинул канал трибуны
-    # [ ] Участник зашел в канал трибуны
-    # [ ] Трибуна закрыта
-    # [ ] Трибуна открыта
-    # [ ] Сообщения были очищены
-    # [ ] Участник был размьючен
-    # [ ] Участник был замьючен
-    # [ ] Участник был разбанен
-    # [ ] Участник был изгнан
     
     @commands.Cog.listener()
     async def on_member_join(self, member):
