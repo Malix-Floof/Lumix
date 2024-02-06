245693d389ffde216f68628749ce091d3dbaf0eb
Malix <101697781+Malix-Floof@users.noreply.github.com>
2024-02-06 17:41:46 +0300

    Update presence.py

--- cogs/presence.py
+++ cogs/presence.py
@@ -31,17 +31,14 @@ class Presence(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
-        self.update_stats.start()
-
-    @tasks.loop(minutes=1)
-    async def update_stats(self):
-        servers = len(self.bot.guilds)
-        serverstr = (
-            str(servers) if servers < 1000
-            else f'{round(servers / 1000, 1)}k' if servers < 1000000
-            else f'{round(servers / 1000000, 1)}M'
+        await self.bot.change_presence(
+            activity=disnake.Activity(
+                type=disnake.ActivityType.custom, 
+                name='✨ Hi, i\'m Lumix!', 
+                state="✨ Hi, i\'m Lumix!"
+            ),
         )
-        await self.bot.change_presence(activity=disnake.Activity(name=f'за {serverstr} серверами', type=disnake.ActivityType.watching))
 
 def setup(bot):
     bot.add_cog(Presence(bot))
+
